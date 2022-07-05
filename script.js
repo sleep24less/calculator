69593632f0a101aa6598f14feb83db767a53dd30
sleepless <arnas.manucan@gmail.com>
2022-07-05 15:02:23 +0300

    Add math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+let result = 0;
+
+
+function add(a, b) {
+    result = a + b;
+    console.log(result);
+}
+
+function subtract(a, b) {
+    result = a - b;
+    console.log(result);
+}
+
+function multiply(a, b) {
+    result = a * b;
+    console.log(result);
+}
+
+function divide(a, b) {
+    result = a / b;
+    console.log(result);
+}
